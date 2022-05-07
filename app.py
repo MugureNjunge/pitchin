4307ef46cffdd57959f8db157752a05e31a9ed84
MugureNjunge <maureennjunge22@gmail.com>
2022-05-07 17:11:15 +0300

    add template folder with html files

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template, url_for
+from forms import SignupForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '567ygh345gbn9765klm2'
 
 posts = [
   {
+    'title' :'Interview Pitch',
+    'content':'Preparing for that important inteview ,gauge just how ready you are. All the very best.'
     
-    'content':'Preparing for that important inteview - lets gauge just how ready you are!',
-    'title' :'Interview Pitch'
   },
   {
-    'content':'The investors you have been cozing upto are a pitch away!',
-    'title' :'Business Proposal'
+    'title' :'Business Proposal',
+    'content':'The investors you have been cozing upto are a pitch away.'
+    
+  },
+  {
+    'title' :'Get that groove on!',
+    'content':'Going on a date with that special someone ,well lets get you date ready.'
+    
+  },
+  {
+    'title' :'Just for fun pitch!',
+    'content':'Wamma learn how to make a great pitch in under 60 seconds?Start now and thank me later.'
 
-  },
-  {
-    'content':'Going on a date with that special someone - well lets get you date ready!',
-    'title' :'Date PItch'
   }
 ]
 
@@ -25,6 +34,17 @@ def home():
 @app.route("/about")
 def about():
   return render_template("about.html", title = about) 
+
+@app.route("/signup")
+def signup():
+  form = SignupForm()
+  return render_template("signup.html", title = 'Signup', form=form)   
+
+@app.route("/login")
+def login():
+  form = LoginForm()
+  return render_template("login.html", title = 'Login', form=form)   
+
 
 if  __name__ == '__main__':
   app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField, SubmitField,BooleanField
+from wtforms.validators import DataRequired,Length, Email,EqualTo
+
+class SignupForm(FlaskForm):
+  username =StringField ('Username', validators= [DataRequired(),Length(min=3, max=12)])
+  email = StringField('Email', validators=[DataRequired(), Email])
+  password= PasswordField('Password', validators=[DataRequired(), Email])
+  confirm_password= PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('Password')])
+  submit=SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+  email = StringField('Email', validators=[DataRequired(), Email])
+  password= PasswordField('Password', validators=[DataRequired(), Email])
+  rememeber = BooleanField('Remember Me')
+  submit=SubmitField('Log in')
