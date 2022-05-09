29b03b542830e7a8e55f897a881f8c0e593858b4
MugureNjunge <maureennjunge22@gmail.com>
2022-05-09 10:56:03 +0300

    create my logout and account routes

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5a833362f6d90268a308cfd3b2dea7ea'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
-login_manager.login_message_category = 'info'
+
 
 
 from app import routes
